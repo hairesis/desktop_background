5ac70d1a7cfccc98f83b9c03b4fa57944a5f2811
Davide Monfrecola <trampfox@gmail.com>
2015-08-17 23:32:45 +0200

    mac osx support added using appscript

--- eb.py
+++ eb.py
@@ -5,13 +5,22 @@ import images
 import urllib
 import os
 import random
+import sys
 
-from gi.repository import Gio
+if sys.platform == "darwin":
+    import subprocess
+    SCRIPT = """/usr/bin/osascript<<END
+    tell application "Finder"
+    set desktop picture to POSIX file "%s"
+    end tell
+    END"""
+else:
+    from gi.repository import Gio
+    _gsettings = Gio.Settings.new(config.SCHEMA)
 
 ## TODO: support osx
 ## http://stackoverflow.com/questions/431205/how-can-i-programatically-change-the-background-in-mac-os-x
 
-_gsettings = Gio.Settings.new(config.SCHEMA)
 
 BASE_PATH = 'file://%s'
 IMG = "%s.jpg"
@@ -28,10 +37,14 @@ def get_random_image():
         download_image(img_file)
     return img_file
 
-
 def set_background():
-    _gsettings.set_string(
-        config.KEY, BASE_PATH % config.CACHE_PATH % get_random_image())
+    background_file = BASE_PATH % config.CACHE_PATH % get_random_image()
+
+    if sys.platform == "darwin":
+        subprocess.Popen(SCRIPT % background_file, shell=True)
+    else:
+        _gsettings.set_string(
+            config.KEY, background_image)
 
 
 if __name__ == '__main__':
