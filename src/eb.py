c1d3640818a53b723ff838bb87851268f4859ae6
Davide Monfrecola <trampfox@gmail.com>
2015-08-18 22:02:08 +0200

    GnomeBgManager and OsxBgManager classes added

--- src/eb.py
+++ src/eb.py
@@ -2,25 +2,18 @@
 
 from conf import config
 from conf import images
+from gnomebgmanager import GnomeBgManager
+from osxbgmanager import OsxBgManager
+
 import urllib
 import os
 import random
 import sys
 
 if sys.platform == "darwin":
-    import subprocess
-    SCRIPT = """/usr/bin/osascript<<END
-    tell application "Finder"
-    set desktop picture to POSIX file "%s"
-    end tell
-    END"""
+    bgmanager = OsxBgManager()
 else:
-    from gi.repository import Gio
-    _gsettings = Gio.Settings.new(config.SCHEMA)
-
-
-BASE_PATH = 'file://%s'
-IMG = "%s.jpg"
+    bgmanager = GnomeBgManager()
 
 
 def download_image(img_file):
@@ -29,21 +22,15 @@ def download_image(img_file):
 
 
 def get_random_image():
-    img_file = IMG % random.choice(images.image_ids)
+    img_file = config.IMG % random.choice(images.image_ids)
     if not os.path.isfile(config.CACHE_PATH % img_file):
         download_image(img_file)
     return img_file
 
 
 def set_background():
-    background_file = BASE_PATH % config.CACHE_PATH % get_random_image()
-
-    if sys.platform == "darwin":
-        subprocess.Popen(SCRIPT % background_file, shell=True)
-    else:
-        _gsettings.set_string(
-            config.KEY, background_file)
-
+    background_file = config.BASE_PATH % config.CACHE_PATH % get_random_image()
+    bgmanager.set_background(background_file)
 
 if __name__ == '__main__':
     set_background()
