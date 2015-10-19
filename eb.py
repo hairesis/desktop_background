cb97e58c22fe0f4614a89388ec2ecc1b3aaa4a68
Andrea Baglioni <0x41ndrea@gmail.com>
2015-10-19 22:04:24 +0200

    added commandline swithch between plugin

--- eb.py
+++ eb.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python
 
-from background.flikr import get_image
+import logging
+import importlib
 from background.managers import bgmanager
 
-__version__ = '1-earth'
+logging.basicConfig()
+
+logger = logging.getLogger('desktop_background:main')
+logger.setLevel(logging.INFO)
+
+__version__ = '1-flikr'
+
+
+def get_plugin(name='earth'):
+    return importlib.import_module('background.%s' % name)
+
+plugin = get_plugin()
+
+
+def _parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source',
+                        nargs='?', default='earth',
+                        type=str, help='flikr earth')
+    args = parser.parse_args()
+    return args.source
 
 
 def set_background():
-    bgmanager.set_background(get_image())
+    bgmanager.set_background(plugin.get_image())
+
 
 if __name__ == '__main__':
+    plugin_name = _parse_arguments() or 'earth'
+    try:
+        logger.info(
+            "chnging desktop background using '%s' plugin" % plugin_name)
+        plugin = get_plugin(plugin_name)
+    except ImportError as ex:
+        logger.warning(
+            "No '%s' plugin found, fallback to default: 'earth'" % plugin_name)
     set_background()
